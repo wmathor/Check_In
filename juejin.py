4e534a4cd3f527ddb44091e8b643fef67be34430
wmathor <739616037@qq.com>
2021-10-11 15:43:33 +0800

    v1.0

--- juejin.py
+++ juejin.py
@@ -3,25 +3,49 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from PIL import Image
+from util import *
 
 username = '15549457220'
 password = 'w739616037'
 
-def Sliding_Captcha():
+def Sliding_Captcha(driver):
+    # 获取验证图片
+    slide_img = driver.find_element_by_xpath("//*[@id='captcha-verify-image']")
+    backgroud_img = driver.find_element_by_xpath("//*[@class='captcha_verify_img_slide react-draggable sc-VigVT ggNWOG']")
+    slide_url = slide_img.get_attribute("src")
+    backgroud_url = backgroud_img.get_attribute("src")
     
+    track = Track()
+    result = track.get_track(slide_url, backgroud_url)
 
+    verify_div = driver.find_element(By.XPATH, '''//div[@class="sc-kkGfuU bujTgx"]''')
+
+    # 按下鼠标左键
+    ActionChains(driver).click_and_hold(verify_div).perform()
+    time.sleep(0.5)
+    # 遍历轨迹进行滑动
+    for t in result:
+        time.sleep(0.01)
+        ActionChains(driver).move_by_offset(xoffset=t, yoffset=0).perform()
+    # 释放鼠标
+    ActionChains(driver).release(on_element=verify_div).perform()
+
+    
 def juejin(driver):
-    driver = webdriver.Chrome()
-    driver.get("https://juejin.cn/")
-    driver.find_element_by_xpath("//*[@class='login-button']").click() # 点击"登录"按钮
-    driver.find_element_by_xpath("//*[@class='login-button']").click() # 点击"其他登录方式"
-    driver.find_element_by_xpath("//*[@name='loginPhoneOrEmail']").send_keys(username)
-    driver.find_element_by_xpath("//*[@name='loginPassword']").send_keys(password)
-    driver.find_element_by_xpath("//*[@class='btn']").click() # 点击"登录"按钮
+    try:
+        
+        driver.find_element_by_xpath("//*[@class='login-button']").click() # 点击"登录"按钮
+        driver.find_element_by_xpath("//*[@class='clickable']").click() # 点击"其他登录方式"
+        driver.find_element_by_xpath("//*[@name='loginPhoneOrEmail']").send_keys(username)
+        driver.find_element_by_xpath("//*[@name='loginPassword']").send_keys(password)
+        driver.find_element_by_xpath("//*[@class='btn']").click() # 点击"登录"按钮
+        
+        # 验证码处理
+        Sliding_Captcha(driver)
 
-    # 验证码处理
-
+    finally:
+        driver.quit()
 
 if __name__ == '__main__':
-    # driver = get_web_driver()
+    driver = get_web_driver()
     juejin(driver)
--- util.py
+++ util.py
@@ -1,10 +1,12 @@
 from PIL import Image
-import os, sys, time, ddddocr, requests
+import cv2, numpy as np
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+import os, sys, time, ddddocr, requests
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
@@ -44,3 +46,99 @@ def Ocr_Captcha(driver, locator, img_path): # 验证码识别
         img_bytes = f.read()
     res = ocr.classification(img_bytes)
     return res
+
+class Track(object):
+    # 处理前图片
+    slider = "./slider.png"
+    background = "./background.png"
+
+    # 将处理之后的图片另存
+    slider_bak = "./slider_bak.png"
+    background_bak = "./background_bak.png"
+
+    def get_track(self, slider_url, background_url) -> list:
+        distance = self.get_slide_distance(slider_url, background_url)
+        result = self.gen_normal_track(distance)
+        return result
+
+    @staticmethod
+    def gen_normal_track(distance):
+        def norm_fun(x, mu, sigma):
+            pdf = np.exp(-((x - mu) ** 2) / (2 * sigma ** 2)) / (sigma * np.sqrt(2 * np.pi))
+            return pdf
+
+        result = []
+        for i in range(-10, 10, 1):
+            result.append(norm_fun(i, 0, 1) * distance)
+        result.append(sum(result) - distance)
+        return result
+
+    @staticmethod
+    def gen_track(distance):  # distance为传入的总距离
+        # 移动轨迹
+        result = []
+        # 当前位移
+        current = 0
+        # 减速阈值
+        mid = distance * 4 / 5
+        # 计算间隔
+        t = 0.2
+        # 初速度
+        v = 1
+
+        while current < distance:
+            if current < mid:
+                # 加速度为2
+                a = 4
+            else:
+                # 加速度为-2
+                a = -3
+            v0 = v
+            # 当前速度
+            v = v0 + a * t
+            # 移动距离
+            move = v0 * t + 1 / 2 * a * t * t
+            # 当前位移
+            current += move
+            # 加入轨迹
+            result.append(round(move))
+        return result
+
+    @staticmethod
+    def onload_save_img(slider_url, slider):
+        r = requests.get(slider_url)
+        with open(slider, 'wb') as f:
+            f.write(r.content)
+
+    def get_slide_distance(self, slider_url, background_url):
+
+        # 下载验证码背景图,滑动图片
+        self.onload_save_img(slider_url, self.slider)
+        self.onload_save_img(background_url, self.background)
+        # 读取进行色度图片，转换为numpy中的数组类型数据
+        slider_pic = cv2.imread(self.slider, 0)
+        background_pic = cv2.imread(self.background, 0)
+        # 获取缺口图数组的形状 -->缺口图的宽和高
+        width, height = slider_pic.shape[::-1]
+
+        cv2.imwrite(self.background_bak, background_pic)
+        cv2.imwrite(self.slider_bak, slider_pic)
+        # 读取另存的滑块图
+        slider_pic = cv2.imread(self.slider_bak)
+        # 进行色彩转换
+        slider_pic = cv2.cvtColor(slider_pic, cv2.COLOR_BGR2GRAY)
+        # 获取色差的绝对值
+        slider_pic = abs(255 - slider_pic)
+        # 保存图片
+        cv2.imwrite(self.slider_bak, slider_pic)
+        # 读取滑块
+        slider_pic = cv2.imread(self.slider_bak)
+        # 读取背景图
+        background_pic = cv2.imread(self.background_bak)
+        # 比较两张图的重叠区域
+        result = cv2.matchTemplate(slider_pic, background_pic, cv2.TM_CCOEFF_NORMED)
+        # 获取图片的缺口位置
+        top, left = np.unravel_index(result.argmax(), result.shape)
+        # 背景图中的图片缺口坐标位置
+        print("当前滑块的缺口位置：", (left, top, left + width, top + height))
+        return left * 340 / 552
