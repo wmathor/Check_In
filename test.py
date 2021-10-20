02e877159bf8265d0ab38bc5d5efc9cb06cedf52
wmathor <739616037@qq.com>
2021-10-20 11:48:31 +0800

    v1.1

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ username = sys.argv[1] # 登录账号
 password = sys.argv[2] # 登录密码
 img_path = os.getcwd() + "/1.png"
 
-@retry(stop_max_attempt_number=5)
+# @retry(stop_max_attempt_number=5)
 def moyupai():
     try:
-        driver = get_web_driver()
+        driver = driver = webdriver.Chrome()
+        driver.implicitly_wait(10)
         driver.get("https://pwl.icu/login")
         driver.find_element_by_xpath("//*[@id='nameOrEmail']").send_keys(username)
         driver.find_element_by_xpath("//*[@id='loginPassword']").send_keys(password)
