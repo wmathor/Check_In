2933f9c030663285503485254a55a276ffd93f2a
mathor <32392878+wmathor@users.noreply.github.com>
2021-11-09 10:19:59 +0800

    Update 91.py

--- 91.py
+++ 91.py
@@ -4,7 +4,7 @@ username = sys.argv[1] # 登录账号
 password = sys.argv[2] # 登录密码
 img_path = os.getcwd() + "/1.png"
 
-@retry(stop_max_attempt_number=2)
+@retry(stop_max_attempt_number=5)
 def moyupai():
     try:
         driver = get_web_driver()
