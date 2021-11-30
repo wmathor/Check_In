c582b4c79cf7faf84f7f7870fde4e01b2e978aab
mathor <32392878+wmathor@users.noreply.github.com>
2021-11-30 10:34:26 +0800

    Update 91.py

--- 91.py
+++ 91.py
@@ -17,7 +17,7 @@ def check_in_91():
             valid = Ocr_Captcha(driver, "//*[@width='160']", img_path) # 验证码识别
             driver.find_element_by_xpath("//*[@name='seccodeverify']").send_keys(valid)
             driver.find_element_by_xpath("//*[@type='submit']").click()
-            wait(5)
+            time.sleep(5)
         
         driver.find_element_by_xpath("//*[@id='dcsignin_tips']").click() # 点击'签到' 按钮
         if driver.find_elements_by_xpath("//*[@id='emot_4']") != []:
