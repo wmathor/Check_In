5ad1f33980f8a7676f8da6ca408588981dcc602b
mathor <32392878+wmathor@users.noreply.github.com>
2022-06-21 09:48:13 +0800

    Update hupu.py

--- hupu.py
+++ hupu.py
@@ -1,4 +1,5 @@
 from util import *
+import time
 
 username = sys.argv[1] # 登录账号
 password = sys.argv[2] # 登录密码
@@ -12,7 +13,7 @@ def muacloud():
         driver.find_element_by_xpath("//*[@id='bbs-login-form_username']").send_keys(username)
         driver.find_element_by_xpath("//*[@id='bbs-login-form_password']").send_keys(password)
         driver.find_element_by_xpath("//*[@id='rectBottom']").click()
-        sleep(1)
+        time.sleep(1)
         driver.find_element_by_xpath("//*[@class='ant-btn ant-btn-primary ant-btn-block submit-btn']").click()
         
         print(driver.find_element_by_xpath("//*[@class='userImports']").text)
